89373b589a6c46b656dbf23f70c6a1c91ecdbdb1
Tyler Philbrick <t5j6p9@gmail.com>
2020-06-25 18:45:49 -0700

    Really bad bump allocator
    Like, really really bad.

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,24 +1,29 @@
 
 use alloc::alloc::{GlobalAlloc, Layout};
-use core::ptr::null_mut;
 
 use crate::sync::Mutex;
 
 const HEAP_LEN: usize = 128 * 1024;
 static mut EARLY_HEAP: [u8; HEAP_LEN] = [0u8; HEAP_LEN];
 
-struct EarlyHeap {}
+struct EarlyHeap {
+    index: usize,
+}
 
 impl EarlyHeap {
     const fn new() -> Self {
-        EarlyHeap {}
+        EarlyHeap { index: 0 }
     }
 }
 
 unsafe impl GlobalAlloc for Mutex<EarlyHeap> {
-    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
-        let _alloc = self.lock();
-        null_mut()
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let mut alloc = self.lock();
+        let ptr = EARLY_HEAP.as_mut_ptr();
+        let new_offset = alloc.index + layout.size();
+        let ret = ptr.offset(alloc.index as isize);
+        alloc.index = new_offset;
+        ret
     }
 
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
